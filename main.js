c26fef336f37d5bfc29b93fa9d5eb376962a1830
Stephen Walley <stephendwalley@icloud.com>
2021-07-01 18:00:34 +1000

    Moved script to new file

---
+++ main.js
@@ -0,0 +1,35 @@
+const img = document.querySelector("img");
+
+const getImage = (searchTerm) => {
+    fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=lDkRuc9Z9QMVlg8gGKU7tbZ8MelQjIJe&s=${searchTerm}`,
+        { mode: "cors" }
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response.data.images.original.url);
+            img.src = response.data.images.original.url;
+        })
+        .catch(e => {
+            console.log(e);
+            alert("No Images Found")
+        });
+};
+
+
+const btn = document.getElementById("newGif");
+btn.addEventListener("click", function (e) {
+    e.preventDefault()
+    console.log("clicked");
+    const input = document.getElementById("input");
+    let searchTerm = input.value;
+    console.log(searchTerm)
+    getImage(searchTerm)				
+
+});
+
+window.onload = () => {
+    getImage("dogs");
+}
